ddc91f7b6e6bbc8cc8995a94a0ace9012f0c50da
Vladimir <glazachev.vladimir@gmail.com>
2014-03-06 03:51:31 +0400

    space fix

--- semester_4/homework_3/h3_num6.hs
+++ semester_4/homework_3/h3_num6.hs
@@ -3,5 +3,5 @@ check_brackets = check_brackets' 0 . filter(`elem` "()")
   where  
 	check_brackets' c [] = (c == 0)
 	check_brackets' c (x:xs) 
-	    | (x == '(') = check_brackets' (c + 1) xs
+		| (x == '(') = check_brackets' (c + 1) xs
 		| (x == ')') = check_brackets' (c - 1) xs
