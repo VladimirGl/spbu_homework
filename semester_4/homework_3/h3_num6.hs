ae93127a2a6b951debf011a2fd66403c2710a365
Vladimir <glazachev.vladimir@gmail.com>
2014-03-06 03:53:02 +0400

    homework 3 ready! tabulation fix

--- semester_4/homework_3/h3_num6.hs
+++ semester_4/homework_3/h3_num6.hs
@@ -1,7 +1,7 @@
 check_brackets :: [Char] -> Bool
 check_brackets = check_brackets' 0 . filter(`elem` "()") 
   where  
-	check_brackets' c [] = (c == 0)
-	check_brackets' c (x:xs) 
-		| (x == '(') = check_brackets' (c + 1) xs
-		| (x == ')') = check_brackets' (c - 1) xs
+    check_brackets' c [] = (c == 0)
+    check_brackets' c (x:xs) 
+        | (x == '(') = check_brackets' (c + 1) xs
+        | (x == ')') = check_brackets' (c - 1) xs
